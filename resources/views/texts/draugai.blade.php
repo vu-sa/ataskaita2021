c1eb5dd5cc0af801b1ceb26f4e515387ccc5f69a
Šomka Marius <marius.somka@mif.stud.vu.lt>
2021-05-06 12:24:15 +0300

    8skyrius

--- resources/views/texts/draugai.blade.php
+++ resources/views/texts/draugai.blade.php
@@ -20,6 +20,36 @@
         <img width="200" height=auto src="/img/unica-network-of-universities-from-the-capitals-of-europe-logo-vector.png">
         <img width="200" height=auto src="/img/VILNIUS_RED_RGB.png">
         <img width="200" height=auto src="/img/Valstybinis-studiju-fondas.jpg">
+
+        
+        <img width="200" height=auto src="/img/jbta logo.png">
+        <img width="200" height=auto src="/img/sp_horiz.png">
+        <img width="200" height=auto src="/img/logo_vu.png">
+        <img width="200" height=auto src="/img/ziniuekoforumas.png">
+        <div class = image>
+        <img width="200" height=auto src="/img/biblioteka.png">
+        </div>
         </div>
     </div>
+
+    <hr>
+    <h2 style="text-align: center;"><b>
+    VU SA 2020–2021 m. ataskaitos projektą koordinavo:</b></p>
+    </h2>
+    <h4 style="text-align: center;">Viltė Jankauskaitė </h4>
+
+    <h2 style="text-align: center;"><b>
+    Leidinį maketavo: </b></p>
+    </h2>
+    <h4 style="text-align: center;">Marius Šomka </h4>
+
+    <h2 style="text-align: center;"><b>
+    Tekstą redagavo:  </b></p>
+    </h2>
+    <h4 style="text-align: center;">Paula Zlatkutė<br>Lijana Savickienė  </h4>
+
+    <h2 style="text-align: center;"><b>
+    Tekstą vertė:   </b></p>
+    </h2>
+    <h4 style="text-align: center;">Justė Vardauskaitė<br>Elena Pranevičiūtė<br>Marija Kliocaitė<br>Greta Vaicekauskaitė<br>Paulina Klara Mikėnaitė </h4>
 </section>
